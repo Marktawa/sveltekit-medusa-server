48378a137714d8f5b79655481c1be65e699fe630
Mark Munyaka <markmunyaka@gmail.com>
2024-06-15 14:02:02 +0200

    Initial Commit

--- storefront/src/routes/+page.js
+++
@@ -1,7 +0,0 @@
-/** @type {import(./$types').PageLoad} */
-export async function load({ fetch }) {
-    const res = await fetch(`http://localhost:9000/store/products`);
-    const payload = await res.json();
-    return { payload };
-}
-
